c131259704bf07f8177d00db7caf1afeb54a0775
song-eunsub <midmay1@gmail.com>
2022-08-18 07:30:53 +0900

    Day 2 ; 잠시 짬냄

---
+++ 2.P011-020.py
@@ -0,0 +1,77 @@
+##### Variable  #####
+print("#11")
+print("="*30+"\n")
+price_ss = 50000
+num_ss = 10
+print(price_ss*num_ss)
+
+print("Answer is some what diff")
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+print("="*30+"\n")
+
+print("#12")
+print("="*30+"\n")
+시가총액 = 2980000000000
+현재가 = 50000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+print("="*30+"\n")
+
+print("#13")
+print("="*30+"\n")
+s = "hello"
+t = "python"
+print(s+"!",t)
+print("="*30+"\n")
+
+print("#14")
+print("="*30+"\n")
+print(2+2*3)
+print("="*30+"\n")
+
+print("#15")
+print("="*30+"\n")
+a = 128 ; print(type(a))
+print('a = "132"') ; a="132" ; print(type(a))
+print("="*30+"\n")
+
+print("#16")
+print("="*30+"\n")
+num_str = "720"
+num_int = int(num_str)
+print(type(num_int))
+print("="*30+"\n")
+
+print("#17")
+print("="*30+"\n")
+num = 100
+str_num = str(num)
+print(str_num, type(str_num))
+print("="*30+"\n")
+
+print("#18")
+print("="*30+"\n")
+num_str = "15.79"
+num_float = float(num_str)
+print(num_float, type(num_float))
+print("="*30+"\n")
+
+print("#19")
+print("="*30+"\n")
+year = "2020"
+year = int(year)
+print(year, year-1, year-2, year-3)
+print("="*30+"\n")
+
+print("#20")
+print("="*30+"\n")
+price_per_month = 48584
+num_month = 36
+total_price = price_per_month * num_month
+print(total_price)
+print("="*30+"\n")
+ 
