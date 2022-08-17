2881cb958a1eb575716c0ce13bf242ca521c94f6
song-eunsub <midmay1@gmail.com>
2022-08-18 00:11:58 +0900

    new file

---
+++ 1.P001-010.py
@@ -0,0 +1,60 @@
+##### Print Strings #####
+print("#1")
+print("="*30+"\n")
+print("Hello World")
+print("="*30+"\n")
+
+print("#2")
+print("="*30+"\n")
+print("Mary\'s cosmetics")
+print("="*30+"\n")
+
+print("#3")
+print("="*30+"\n")
+print("신씨가 소리질렀다. \"도둑이야\".")
+print('신씨가 소리질렀다. "도둑이야".')
+print("="*30+"\n")
+
+print("#4")
+print("="*30+"\n")
+print('"C:\\Winows"')
+print('"C:\Windows"')
+print("C:\Windows" + "### maybe mine is better")
+print("="*30+"\n")
+
+print("#5")
+print("="*30+"\n")
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+print("\\n is line break(or make new line) and \\t is tab")
+print("="*30+"\n")
+
+print("#6")
+print("="*30+"\n")
+print("오늘은","일요일")
+print("comma(,) connects two string bunchs and makes one space between them")
+print("="*30+"\n")
+
+print("#7")
+print("="*30+"\n")
+print("naver;kakao;samsung")
+print('answer is "naver","kakao","samsung", sep=";"')
+print("naver","kakao","samsung", sep=";")
+print("="*30+"\n")
+
+print("#8")
+print("="*30+"\n")
+print("naver","kakao","sk","samsung", sep="/")
+print("="*30+"\n")
+
+print("#9")
+print("="*30+"\n")
+print("first", end=" ") ; print("second")
+print("="*30+"\n")
+
+print("#10")
+print("="*30+"\n")
+calc=5/3
+print(calc)
+print('fuxing idiot. if you can print variable, you just write within "print"')
+print(5/3)
+print("="*30+"\n")
