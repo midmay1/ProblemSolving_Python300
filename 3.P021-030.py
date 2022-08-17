07a391a8d545e44089eebc9f36c2f43facf0c7f8
song-eunsub <midmay1@gmail.com>
2022-08-18 08:15:03 +0900

    아침 짬내서

---
+++ 3.P021-030.py
@@ -0,0 +1,58 @@
+##### String indexing  #####
+print("#1")
+print("="*30+"\n")
+letters = 'python'
+print(letters[0],letters[2])
+print("="*30+"\n")
+
+print("#2")
+print("="*30+"\n")
+license_plate = "24가 2210"
+print(license_plate[-4:])
+print("="*30+"\n")
+
+print("#3")
+print("="*30+"\n")
+string="홀짝홀짝홀짝홀짝"
+print(string[::2])
+print("="*30+"\n")
+
+print("#4")
+print("="*30+"\n")
+string="python"
+print('origin string is "{}"'.format(string))
+print("".join(reversed(string)))
+reversed_string = ''
+for item in string:
+	reversed_string = item + reversed_string
+print(reversed_string)
+print(string[::-1])
+
+print('For me the best is : print(string[::-1])')
+print(string[::-1])
+print("="*30+"\n")
+
+print("#5")
+print("="*30+"\n")
+print("="*30+"\n")
+
+print("#6")
+print("="*30+"\n")
+print("="*30+"\n")
+
+print("#7")
+print("="*30+"\n")
+print("="*30+"\n")
+
+print("#8")
+print("="*30+"\n")
+print("="*30+"\n")
+
+print("#9")
+print("="*30+"\n")
+print("="*30+"\n")
+
+print("#10")
+print("="*30+"\n")
+print("="*30+"\n")
+ 
